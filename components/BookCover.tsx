695b2f53f89441d64e5cbea3e79404478a16223e
Nghiem Pham <nghiemph95@gmail.com>
2025-01-15 10:53:02 +0700

    Updated: 15-01-2025 10:53:02

--- components/BookCover.tsx
+++ components/BookCover.tsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import { cn } from "@/lib/utils";
+import Image from "next/image";
 // import BookCoverSvg from "@/components/BookCoverSvg";
 // import { IKImage } from "imagekitio-next";
 // import config from "@/lib/config";
@@ -38,11 +39,17 @@ const BookCover = ({
       )}
     >
       {/* <BookCoverSvg coverColor={coverColor} /> */}
-
+      BOOK SIDE SVG
       <div
         className="absolute z-10"
         style={{ left: "12%", width: "87.5%", height: "88%" }}
       >
+        <Image
+          src={coverImage}
+          alt="Book cover"
+          fill
+          className="rounded-sm object-fill"
+        />
         {/* <IKImage
           path={coverImage}
           urlEndpoint={config.env.imagekit.urlEndpoint}
