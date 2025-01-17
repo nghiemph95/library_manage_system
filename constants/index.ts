f2ff524a16b8618c87277f0b46eed569cc9e066f
Nghiem Pham <nghiemph95@gmail.com>
2025-01-17 10:51:50 +0700

    Updated: 17-01-2025 10:51:50

--- constants/index.ts
+++ constants/index.ts
@@ -62,13 +62,13 @@ export const sampleBooks = [
     author: "Matt Haig",
     genre: "Fantasy / Fiction",
     rating: 4.6,
-    total_copies: 20,
-    available_copies: 10,
+    totalCopies: 20,
+    availableCopies: 10,
     description:
       "A dazzling novel about all the choices that go into a life well lived, The Midnight Library tells the story of Nora Seed as she finds herself between life and death.",
-    color: "#1c1f40",
-    cover: "https://m.media-amazon.com/images/I/81J6APjwxlL.jpg",
-    video: "/sample-video.mp4?updatedAt=1722593504152",
+    coverColor: "#1c1f40",
+    coverUrl: "https://m.media-amazon.com/images/I/81J6APjwxlL.jpg",
+    videoUrl: "/sample-video.mp4?updatedAt=1722593504152",
     summary:
       "A dazzling novel about all the choices that go into a life well lived, The Midnight Library tells the story of Nora Seed as she finds herself between life and death. A dazzling novel about all the choices that go into a life well lived, The Midnight Library tells the story of Nora Seed as she finds herself between life and death.",
   },
