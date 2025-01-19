1aed8729a0916c2e8943d7e0718f971eb9caeeec
Nghiem Pham <nghiemph95@gmail.com>
2025-01-19 21:02:56 +0700

    Updated: 19-01-2025 21:02:56

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -24,7 +24,7 @@ import { Input } from "@/components/ui/input";
 import Link from "next/link";
 import { FIELD_NAMES, FIELD_TYPES } from "@/constants";
 import FileUpload from "@/components/FileUpload";
-// import { toast } from "@/hooks/use-toast";
+import { toast } from "@/hooks/use-toast";
 import { useRouter } from "next/navigation";
 
 interface Props<T extends FieldValues> {
