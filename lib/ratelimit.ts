9224dfdc7cae0cc6b2f6fe0e58a86ce90b0ff959
Nghiem Pham <nghiemph95@gmail.com>
2025-01-20 15:57:55 +0700

    Updated: 20-01-2025 15:57:55

--- lib/ratelimit.ts
+++ lib/ratelimit.ts
@@ -3,7 +3,7 @@ import redis from "@/database/redis";
 
 const ratelimit = new Ratelimit({
   redis,
-  limiter: Ratelimit.fixedWindow(5, "1m"),
+  limiter: Ratelimit.fixedWindow(5, "1m"), // 5 requests per minute
   analytics: true,
   prefix: "@upstash/ratelimit",
 });
